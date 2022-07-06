12722977a48f1c80cb9dbd8636e10bd38722d564
Yonghoon <yonghoonj98@gmail.com>
2022-07-06 16:06:58 +0900

    2강 완료

--- lecture/끝말읽기/WordRelay.jsx
+++ lecture/끝말읽기/WordRelay.jsx
@@ -1,14 +1,40 @@
 const React = require('react');
-const { Component } = React;
+const { useState, useRef } = React;
 
-class WordRelay extends Component {
-  state = {
-    text: 'hello, world',
+const WordRelay = () => {
+  const [word, setWord] = useState('제로초');
+  const [value, setValue] = useState('');
+  const [result, setResult] = useState('');
+  const inputEl = useRef(null);
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    if (word[word.length - 1] === value[0]) {
+      setResult('딩동댕');
+      setWord(value);
+      setValue('');
+      inputEl.current.focus();
+    } else {
+      setResult('땡');
+      setValue('');
+      inputEl.current.focus();
+    }
   };
 
-  render() {
-    return <h1>{this.state.text}</h1>;
-  }
-}
+  return (
+    <>
+      <div>{word}</div>
+      <form onSubmit={onSubmitForm}>
+        <input
+          ref={inputEl}
+          value={value}
+          onChange={(e) => setValue(e.currentTarget.value)}
+        />
+        <button>입력!</button>
+      </form>
+      <div>{result}</div>
+    </>
+  );
+};
 
 module.exports = WordRelay;
--- lecture/끝말읽기/webpack.config.js
+++ lecture/끝말읽기/webpack.config.js
@@ -1,11 +1,10 @@
 const path = require('path');
-const { webpack } = require('webpack');
-// const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
   name: 'word-relay-dev',
   mode: 'development',
-  devtool: 'inline-source-map',
+  devtool: 'eval',
 
   // 확장자를 설정할 수 있음. 작성한 확장자를 찾음
   resolve: {
@@ -24,20 +23,39 @@ module.exports = {
         test: /\.jsx?/,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
-          plugins: ['@babel/plugin-proposal-class-properties'],
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  browsers: ['> 1% in KR'],
+                },
+                debug: true,
+              },
+            ],
+            '@babel/preset-react',
+          ],
+          plugins: [
+            '@babel/plugin-proposal-class-properties',
+            'react-refresh/babel',
+          ],
         },
       },
     ],
   },
 
   // 확장 프로그램
-  plugins: [new webpack.LoaderOptionsPlugin({ debug: true })],
+  plugins: [new ReactRefreshWebpackPlugin()],
 
   // 출력 설정
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'app.js',
-    publicPath: '/dist',
+    publicPath: '/dist/',
+  },
+  devServer: {
+    devMiddleware: { publicPath: '/dist/' },
+    static: { directory: path.resolve(__dirname) },
+    hot: true,
   },
 };
