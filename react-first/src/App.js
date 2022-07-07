3a1292e84746a2519eb1b28fe413a3aac38ad262
Yonghoon <yonghoonj98@gmail.com>
2022-07-07 15:36:43 +0900

    ToDo 과제 퍼블리싱 완료

--- react-first/src/App.js
+++ react-first/src/App.js
@@ -1,36 +1,72 @@
-import logo from "./logo.svg";
-import "./App.css";
-import React, { useState } from "react";
+import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  const [a, setA] = useState([1, 2, 3, 4, 5]);
-  const plusNumber = () => {
-    const arr = [...a];
-    setA([...arr]);
+  const [inputs, setInputs] = useState({
+    name: '',
+    nickname: '',
+  });
+  const [inputContent, setInputContent] = useState('');
+
+  const onChange = (e) => {
+    const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
+    setInputs({
+      ...inputs, // 기존의 input 객체를 복사한 뒤
+      [name]: value, // name 키를 가진 값을 value 로 설정
+    });
   };
 
+  const addToDoList = (e) => {
+    const content = e.target;
+    console.log(content);
+    setInputContent(content);
+  };
+  // const [a, setA] = useState([1, 2, 3, 4, 5]);
+  // const plusNumber = () => {
+  //   const arr = [...a];
+  //   setA([...arr]);
+  // };
+  const test = () => {
+    // add 눌럿을 때 실행할 애들
+  };
+  // <input name="name" placeholder="이름" onChange={onChange} value={name} />
+  // <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
+  // <button onClick={onReset}>초기화</button>
+
   return (
-    // Fragment는 빈태그 명시
-    <React.Fragment>
-      <h1>{a}</h1>
-      <button onClick={plusNumber}>plus</button>
-    </React.Fragment>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <div className="ToDoList">
+      <div className="ToDoTitle">
+        <div className="Title">ToDoList</div>
+      </div>
+      <div className="ToDoInput">
+        <div>
+          <input
+            className="InputBox"
+            onChange={onChange}
+            value={content}
+            spellcheck="false"
+          />
+        </div>
+        <div>
+          <button onClick={test} className="InputBtn">
+            add
+          </button>
+        </div>
+      </div>
+      <div>
+        <div className="ToDoBox">
+          <div className="ToDoDelete">X</div>
+          <div className="ToDoContent">할 일 1</div>
+          <div>
+            <button className="UpdateBtn">수정</button>
+          </div>
+        </div>
+      </div>
+      {/* <React.Fragment>
+        <h1>{a}</h1>
+        <button onClick={plusNumber}>plus</button>
+      </React.Fragment> */}
+    </div>
   );
 }
 
