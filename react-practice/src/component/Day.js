e6d973ab9006f16e6c6fcafe32b047929a240ed4
YH <acg@ACGui-MacBookPro.local>
2022-08-25 15:46:07 +0900

    Feature: 예외처리

--- react-practice/src/component/Day.js
+++ react-practice/src/component/Day.js
@@ -1,11 +1,13 @@
+import { useParams } from "react-router-dom";
 import dummy from "../db/data.json";
 
 export default function Day() {
-  const day = 2;
-  const wordList = dummy.words.filter((word) => word.day === day);
+  const { day } = useParams();
+  const wordList = dummy.words.filter((word) => word.day === Number(day));
 
   return (
     <>
+      <h2>Day {day}</h2>
       <table>
         <tbody>
           {wordList.map((word) => (
