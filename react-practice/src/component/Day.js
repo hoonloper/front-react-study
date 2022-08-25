771501ac97786dc9d54876a40bd87a8ba3db5560
YH <acg@ACGui-MacBookPro.local>
2022-08-25 16:44:27 +0900

    Feature: json server 적용

--- react-practice/src/component/Day.js
+++ react-practice/src/component/Day.js
@@ -4,6 +4,7 @@ import Word from "./Word";
 
 export default function Day() {
   const { day } = useParams();
+
   const wordList = dummy.words.filter((word) => word.day === Number(day));
 
   return (
