746db73a2ab8a26b104e153ce8dcc6c7d0dc5d94
YH <yonghoonj98@gmail.com>
2022-09-02 15:18:46 +0900

    Material 연습

---
+++ react-go/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Footer from "./component/Footer/Footer";
+import Header from "./component/Header/Header";
+import List from "./component/List/List";
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <List></List>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
